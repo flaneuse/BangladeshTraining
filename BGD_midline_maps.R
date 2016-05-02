c7a10de198087587fb27c99b49796546c5d7450d
flaneuse <laura.d.hughes@gmail.com>
2016-05-01 21:33:26 -0400

    change choropleth

--- BGD_midline_maps.R
+++ BGD_midline_maps.R
@@ -1,17 +1,18 @@
 source('~/GitHub/Misc. projects/Import shapefile/shp2csv.R')
 
-adm2 = shp2csv(workingDir = '~/Documents/USAID/Bangladesh/Training/BGD_Adm2/',
+adm2_map = shp2csv(workingDir = '~/Documents/USAID/Bangladesh/Training/BGD_Adm2/',
                layerName = 'DistrictA',
                labelVar = 'District',
                exportData = FALSE)
 
 # Pull out the lat/lon data
-adm2_df = adm2$df
+adm2_df = adm2_map$df
 
 
 # merge in arsenic data ---------------------------------------------------
 adm2_df = left_join(adm2_df, adm2,
-                    by = c("District" = "district"))
+                    by = c("District" = "district")) %>% 
+  mutate(chgAs50 = meanAs50ppb.y - meanAs50ppb.x)
 
 
 # plot map ----------------------------------------------------------------
@@ -19,11 +20,12 @@ adm2_df = left_join(adm2_df, adm2,
 p = ggplot(adm2_df, aes(x = long, 
                    y = lat, 
                    group = group, 
-                   fill = meanAs50ppb)) +
+                   fill = meanAs50ppb.y)) +
   geom_polygon() +
   theme_void() + 
   coord_equal() +
-  scale_fill_gradientn(colours = brewer.pal(9, 'YlGnBu')) +
+  scale_fill_gradientn(colours = brewer.pal(9, 'YlOrBr'),
+                       limits = c(0, 0.9)) +
   theme(legend.position = 'none')
 
 fileName = '~/Documents/USAID/Bangladesh/Training/Training docs/BGD_midlineAs_choro.pdf'
@@ -38,3 +40,58 @@ ggsave(filename = fileName,
        useDingbats=FALSE,
        compress = FALSE,
        dpi = 300)
+
+
+# baseline map ------------------------------------------------------------
+
+p = ggplot(adm2_df, aes(x = long, 
+                        y = lat, 
+                        group = group, 
+                        fill = meanAs50ppb.x)) +
+  geom_polygon() +
+  theme_void() + 
+  coord_equal() +
+  scale_fill_gradientn(colours = brewer.pal(9, 'YlOrBr'),
+                       limits = c(0, 0.9)) +
+  theme(legend.position = 'none')
+
+fileName = '~/Documents/USAID/Bangladesh/Training/Training docs/BGD_baselineAs_choro.pdf'
+plotWidth = 5
+plotHeight = 6
+
+ggsave(filename = fileName,
+       width = plotWidth, height = plotHeight,
+       bg = 'transparent',
+       paper = 'special',
+       units = 'in',
+       useDingbats=FALSE,
+       compress = FALSE,
+       dpi = 300)
+
+
+# change ------------------------------------------------------------------
+
+p = ggplot(adm2_df, aes(x = long, 
+                        y = lat, 
+                        group = group, 
+                        fill = chgAs50)) +
+  geom_polygon() +
+  theme_void() + 
+  coord_equal() +
+  scale_fill_gradientn(colours = rev(brewer.pal(9, 'RdYlBu')),
+                       limits = c(-0.9, 0.9)) +
+  theme(legend.position = 'none')
+
+fileName = '~/Documents/USAID/Bangladesh/Training/Training docs/BGD_changeAs_choro.pdf'
+plotWidth = 5
+plotHeight = 6
+
+ggsave(filename = fileName,
+       width = plotWidth, height = plotHeight,
+       bg = 'transparent',
+       paper = 'special',
+       units = 'in',
+       useDingbats=FALSE,
+       compress = FALSE,
+       dpi = 300)
+
